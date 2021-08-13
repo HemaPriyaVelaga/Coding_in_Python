a326eb24bb93abfe5a60d70096f4cffa3a5a602c
Hema Priya Velaga <hemapriyavelaga@gmail.com>
2021-08-14 00:04:13 +0530

    Search BST recursively (LeetCode Recursion card)

---
+++ Recursion/BST_Search.py
@@ -0,0 +1,13 @@
+def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        if root == None:
+            return None
+        
+        if root.val == val:
+            return root
+        
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        
+        else:
+            return self.searchBST(root.right, val)
